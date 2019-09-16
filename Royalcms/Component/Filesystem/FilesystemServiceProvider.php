83c145decf6e164c7c47d824ea04ffc5eb9b64fd
王正东 <wangzhengdong@ecmoban.com>
2019-09-16 13:51:25 +0800

    更新Filesystem组件

--- Royalcms/Component/Filesystem/FilesystemServiceProvider.php
+++ Royalcms/Component/Filesystem/FilesystemServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace Royalcms\Component\Filesystem;
 
-class FilesystemServiceProvider extends \Illuminate\Support\ServiceProvider
+class FilesystemServiceProvider extends \Illuminate\Filesystem\FilesystemServiceProvider
 {
 
     /**
@@ -12,9 +12,10 @@ class FilesystemServiceProvider extends \Illuminate\Support\ServiceProvider
      */
     public function register()
     {
+        $this->loadAlias();
+
         parent::register();
 
-        $this->loadAlias();
     }
 
     /**
@@ -22,13 +23,11 @@ class FilesystemServiceProvider extends \Illuminate\Support\ServiceProvider
      */
     protected function loadAlias()
     {
-        $this->royalcms->booting(function() {
-            $loader = \Royalcms\Component\Foundation\AliasLoader::getInstance();
-            $loader->alias('Royalcms\Component\Filesystem\ClassFinder', 'Illuminate\Filesystem\ClassFinder');
-            $loader->alias('Royalcms\Component\Filesystem\Filesystem', 'Illuminate\Filesystem\Filesystem');
-            $loader->alias('Royalcms\Component\Filesystem\FilesystemAdapter', 'Illuminate\Filesystem\FilesystemAdapter');
-            $loader->alias('Royalcms\Component\Filesystem\FilesystemManager', 'Illuminate\Filesystem\FilesystemManager');
-        });
+        $loader = \Royalcms\Component\Foundation\AliasLoader::getInstance();
+//        $loader->alias('Royalcms\Component\Filesystem\ClassFinder', 'Illuminate\Filesystem\ClassFinder');
+        $loader->alias('Royalcms\Component\Filesystem\Filesystem', 'Illuminate\Filesystem\Filesystem');
+        $loader->alias('Royalcms\Component\Filesystem\FilesystemAdapter', 'Illuminate\Filesystem\FilesystemAdapter');
+        $loader->alias('Royalcms\Component\Filesystem\FilesystemManager', 'Illuminate\Filesystem\FilesystemManager');
     }
 
 
