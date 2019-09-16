36b0f35932a91871dda77a58f2bceebe1dab8830
王正东 <wangzhengdong@ecmoban.com>
2019-09-16 13:52:37 +0800

    更新Support组件

--- Royalcms/Component/Support/SupportServiceProvider.php
+++ Royalcms/Component/Support/SupportServiceProvider.php
@@ -6,6 +6,26 @@ class SupportServiceProvider extends \Illuminate\Support\ServiceProvider
 {
 
     /**
+     * The application instance.
+     *
+     * @var \Royalcms\Component\Contracts\Foundation\Royalcms
+     */
+    protected $royalcms;
+
+    /**
+     * Create a new service provider instance.
+     *
+     * @param  \Royalcms\Component\Contracts\Foundation\Royalcms  $royalcms
+     * @return void
+     */
+    public function __construct($royalcms)
+    {
+        parent::__construct($royalcms);
+
+        $this->royalcms = $royalcms;
+    }
+
+    /**
      * Register the service provider.
      *
      * @return void
