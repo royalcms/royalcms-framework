7edaca0485bb3ef460aa5cfa88fb95abea2afc26
王正东 <wangzhengdong@ecmoban.com>
2019-09-16 13:52:06 +0800

    更新Log组件

--- Royalcms/Component/Log/LogServiceProvider.php
+++ Royalcms/Component/Log/LogServiceProvider.php
@@ -2,16 +2,29 @@
 
 namespace Royalcms\Component\Log;
 
-use Royalcms\Component\Support\ServiceProvider;
 
-class LogServiceProvider extends ServiceProvider {
+class LogServiceProvider extends \Illuminate\Log\LogServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool 
-	 */
-	protected $defer = true;
+    /**
+     * The application instance.
+     *
+     * @var \Royalcms\Component\Contracts\Foundation\Royalcms
+     */
+    protected $royalcms;
+
+    /**
+     * Create a new service provider instance.
+     *
+     * @param  \Royalcms\Component\Contracts\Foundation\Royalcms  $royalcms
+     * @return void
+     */
+    public function __construct($royalcms)
+    {
+        parent::__construct($royalcms);
+
+        $this->royalcms = $royalcms;
+    }
 
 	/**
 	 * Register the service provider.
@@ -20,29 +33,32 @@ class LogServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
-		$this->royalcms->bind('log.store', function($royalcms) {
-		    return new FileStore(
+	    parent::register();
+
+        $this->registerLogStore();
+
+        $this->registerLogSetup();
+	}
+
+
+    protected function registerLogStore()
+    {
+        $this->royalcms->singleton('log.store', function($royalcms) {
+            return new FileStore(
                 $royalcms['files'], storage_path().'/logs/'
             );
         });
+    }
 
-		// If the setup Closure has been bound in the container, we will resolve it
-		// and pass in the logger instance. This allows this to defer all of the
-		// logger class setup until the last possible second, improving speed.
-		if (isset($this->royalcms['log.setup']))
-		{
-			call_user_func($this->royalcms['log.setup'], $this->royalcms['log']);
-		}
-	}
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('log.store');
-	}
+    protected function registerLogSetup()
+    {
+        // If the setup Closure has been bound in the container, we will resolve it
+        // and pass in the logger instance. This allows this to defer all of the
+        // logger class setup until the last possible second, improving speed.
+        if (isset($this->royalcms['log.setup'])) {
+            call_user_func($this->royalcms['log.setup'], $this->royalcms['log']);
+        }
+    }
 
 }
