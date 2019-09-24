b6ee0109e41f9d1a9e3752c2301c74f0fff8ba1b
王正东 <wangzhengdong@ecmoban.com>
2019-09-24 09:54:46 +0800

    更新Events组件

--- Royalcms/Component/Events/EventServiceProvider.php
+++ Royalcms/Component/Events/EventServiceProvider.php
@@ -8,15 +8,35 @@ use Illuminate\Contracts\Queue\Factory as QueueFactoryContract;
 class EventServiceProvider extends ServiceProvider
 {
     /**
+     * The application instance.
+     *
+     * @var \Royalcms\Component\Contracts\Foundation\Royalcms
+     */
+    protected $royalcms;
+
+    /**
+     * Create a new service provider instance.
+     *
+     * @param  \Royalcms\Component\Contracts\Foundation\Royalcms|\Illuminate\Contracts\Foundation\Application  $royalcms
+     * @return void
+     */
+    public function __construct($royalcms)
+    {
+        parent::__construct($royalcms);
+
+        $this->royalcms = $royalcms;
+    }
+
+    /**
      * Register the service provider.
      *
      * @return void
      */
     public function register()
     {
-        $this->app->singleton('events', function ($app) {
-            return (new Dispatcher($app))->setQueueResolver(function () use ($app) {
-                return $app->make(QueueFactoryContract::class);
+        $this->royalcms->singleton('events', function ($royalcms) {
+            return (new Dispatcher($royalcms))->setQueueResolver(function () use ($royalcms) {
+                return $royalcms->make(QueueFactoryContract::class);
             });
         });
     }
