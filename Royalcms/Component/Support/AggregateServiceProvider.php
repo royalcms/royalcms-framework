4b0df49d83d1c23cc4812ddc945e73f03d9bc714
王正东 <wangzhengdong@ecmoban.com>
2019-09-26 17:52:53 +0800

    修正Support组件

--- Royalcms/Component/Support/AggregateServiceProvider.php
+++ Royalcms/Component/Support/AggregateServiceProvider.php
@@ -2,51 +2,7 @@
 
 namespace Royalcms\Component\Support;
 
-class AggregateServiceProvider extends ServiceProvider
+class AggregateServiceProvider extends \Illuminate\Support\AggregateServiceProvider
 {
-    /**
-     * The provider class names.
-     *
-     * @var array
-     */
-    protected $providers = [];
 
-    /**
-     * An array of the service provider instances.
-     *
-     * @var array
-     */
-    protected $instances = [];
-
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->instances = [];
-
-        foreach ($this->providers as $provider) {
-            $this->instances[] = $this->royalcms->register($provider);
-        }
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        $provides = [];
-
-        foreach ($this->providers as $provider) {
-            $instance = $this->royalcms->resolveProviderClass($provider);
-
-            $provides = array_merge($provides, $instance->provides());
-        }
-
-        return $provides;
-    }
 }
--- Royalcms/Component/Support/SupportServiceProvider.php
+++ Royalcms/Component/Support/SupportServiceProvider.php
@@ -54,6 +54,19 @@ class SupportServiceProvider extends \Illuminate\Support\ServiceProvider
         $loader = \Royalcms\Component\Foundation\AliasLoader::getInstance();
         $loader->alias('Royalcms\Component\Support\Collection', 'Illuminate\Support\Collection');
         $loader->alias('Royalcms\Component\Support\MessageBag', 'Illuminate\Support\MessageBag');
+        $loader->alias('Royalcms\Component\Support\Fluent', 'Illuminate\Support\Fluent');
+        $loader->alias('Royalcms\Component\Support\ViewErrorBag', 'Illuminate\Support\ViewErrorBag');
+        $loader->alias('Royalcms\Component\Support\Pluralizer', 'Illuminate\Support\Pluralizer');
+        $loader->alias('Royalcms\Component\Support\Optional', 'Illuminate\Support\Optional');
+        $loader->alias('Royalcms\Component\Support\NamespacedItemResolver', 'Illuminate\Support\NamespacedItemResolver');
+        $loader->alias('Royalcms\Component\Support\HtmlString', 'Illuminate\Support\HtmlString');
+        $loader->alias('Royalcms\Component\Support\HigherOrderTapProxy', 'Illuminate\Support\HigherOrderTapProxy');
+        $loader->alias('Royalcms\Component\Support\HigherOrderCollectionProxy', 'Illuminate\Support\HigherOrderCollectionProxy');
+        $loader->alias('Royalcms\Component\Support\Carbon', 'Illuminate\Support\Carbon');
+        $loader->alias('Royalcms\Component\Support\Traits\CapsuleManagerTrait', 'Illuminate\Support\Traits\CapsuleManagerTrait');
+        $loader->alias('Royalcms\Component\Support\Traits\Macroable', 'Illuminate\Support\Traits\Macroable');
+
+        $loader->alias('Royalcms\Component\Support\ClassLoader', 'Royalcms\Component\ClassLoader\ClassLoader');
     }
 
 
