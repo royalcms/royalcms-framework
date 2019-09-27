945b29828a5f510775e43bbdc0a1f1de6a883363
王正东 <wangzhengdong@ecmoban.com>
2019-09-27 10:47:46 +0800

    优化Hashing组件

--- Royalcms/Component/Hashing/HashServiceProvider.php
+++ Royalcms/Component/Hashing/HashServiceProvider.php
@@ -2,36 +2,8 @@
 
 namespace Royalcms\Component\Hashing;
 
-use Royalcms\Component\Support\ServiceProvider;
 
-class HashServiceProvider extends ServiceProvider
+class HashServiceProvider extends \Illuminate\Hashing\HashServiceProvider
 {
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = true;
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->royalcms->singleton('hash', function () {
-            return new BcryptHasher;
-        });
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return ['hash'];
-    }
 }
