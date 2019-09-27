1dc7ab99816fb5d42abd09fbae07294305f3b8d7
王正东 <wangzhengdong@ecmoban.com>
2019-09-27 10:47:35 +0800

    优化Encryption组件

--- Royalcms/Component/Encryption/EncryptionServiceProvider.php
+++ Royalcms/Component/Encryption/EncryptionServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Royalcms\Component\Encryption;
 
+use Illuminate\Encryption\Encrypter;
+use Illuminate\Support\Str;
 use RuntimeException;
 use Royalcms\Component\Support\ServiceProvider;
 
@@ -17,16 +19,32 @@ class EncryptionServiceProvider extends ServiceProvider
         $this->royalcms->singleton('encrypter', function ($royalcms) {
             $config = $royalcms->make('config')->get('system');
 
-            $key = $config['auth_key'];
+            // If the key starts with "base64:", we will need to decode the key before handing
+            // it off to the encrypter. Keys may be base-64 encoded for presentation and we
+            // want to make sure to convert them back to the raw bytes before encrypting.
+            if (Str::startsWith($key = $this->key($config), 'base64:')) {
+                $key = base64_decode(substr($key, 7));
+            }
 
-            $cipher = $config['cipher'];
+            return new Encrypter($key, $config['cipher']);
+        });
+    }
 
-            if (Encrypter::supported($key, $cipher)) {
-                return new Encrypter($key, $cipher);
-            } elseif (McryptEncrypter::supported($key, $cipher)) {
-                return new McryptEncrypter($key, $cipher);
-            } else {
-                throw new RuntimeException('No supported encrypter found. The cipher and / or key length are invalid.');
+    /**
+     * Extract the encryption key from the given configuration.
+     *
+     * @param  array  $config
+     * @return string
+     *
+     * @throws \RuntimeException
+     */
+    protected function key(array $config)
+    {
+        return tap($config['auth_key'], function ($key) {
+            if (empty($key)) {
+                throw new RuntimeException(
+                    'No application encryption key has been specified.'
+                );
             }
         });
     }
