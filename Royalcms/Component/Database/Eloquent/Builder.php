4a6827b862e114daa098db7f76496059aa06edf9
王正东 <wangzhengdong@ecmoban.com>
2019-09-30 10:27:24 +0800

    修复数据模型返回结果兼容问题

--- Royalcms/Component/Database/Eloquent/Builder.php
+++ Royalcms/Component/Database/Eloquent/Builder.php
@@ -22,4 +22,40 @@ class Builder extends QueryBuilder
         )->all();
     }
 
+    /**
+     * Execute the query and get the first result.
+     *
+     * @param  array  $columns
+     * @return \Royalcms\Component\Database\Eloquent\Model|static|null
+     */
+    public function first($columns = ['*'])
+    {
+        return $this->take(1)->get($columns)->first();
+    }
+
+    /**
+     * Get an array with the values of a given column.
+     *
+     * @param  string  $column
+     * @param  string|null  $key
+     * @return \Royalcms\Component\Support\Collection
+     */
+    public function lists($column, $key = null)
+    {
+        $results = $this->query->lists($column, $key);
+
+        // If the model has a mutator for the requested column, we will spin through
+        // the results and mutate the values so that the mutated version of these
+        // columns are returned as you would expect from these Eloquent models.
+        if ($this->model->hasGetMutator($column)) {
+            foreach ($results as $key => &$value) {
+                $fill = [$column => $value];
+
+                $value = $this->model->newFromBuilder($fill)->$column;
+            }
+        }
+
+        return collect($results);
+    }
+
 }
