4b7502b837de77e77bda624ec1852ec11cc4622b
王正东 <wangzhengdong@ecmoban.com>
2019-10-11 11:51:38 +0800

    优化Http组件的别名加载

--- Royalcms/Component/Http/HttpServiceProvider.php
+++ Royalcms/Component/Http/HttpServiceProvider.php
@@ -9,7 +9,6 @@ class HttpServiceProvider extends ServiceProvider
 
     /**
      * Register the service provider.
-     *
      * @return void
      */
     public function register()
@@ -24,22 +23,33 @@ class HttpServiceProvider extends ServiceProvider
     protected function loadAlias()
     {
         $loader = \Royalcms\Component\Foundation\AliasLoader::getInstance();
-//            $loader->alias('Royalcms\Component\Http\Exception\HttpResponseException', 'Illuminate\Http\Exception\HttpResponseException');
-//            $loader->alias('Royalcms\Component\Http\Exception\PostTooLargeException', 'Illuminate\Http\Exception\PostTooLargeException');
-        $loader->alias('Royalcms\Component\Http\File', 'Illuminate\Http\File');
-        $loader->alias('Royalcms\Component\Http\FileHelpers', 'Illuminate\Http\FileHelpers');
-        $loader->alias('Royalcms\Component\Http\JsonResponse', 'Illuminate\Http\JsonResponse');
-        $loader->alias('Royalcms\Component\Http\Middleware\CheckResponseForModifications', 'Illuminate\Http\Middleware\CheckResponseForModifications');
-        $loader->alias('Royalcms\Component\Http\Middleware\FrameGuard', 'Illuminate\Http\Middleware\FrameGuard');
-        $loader->alias('Royalcms\Component\Http\RedirectResponse', 'Illuminate\Http\RedirectResponse');
-        $loader->alias('Royalcms\Component\Http\Request', 'Illuminate\Http\Request');
-        $loader->alias('Royalcms\Component\Http\Response', 'Illuminate\Http\Response');
-        $loader->alias('Royalcms\Component\Http\ResponseTrait', 'Illuminate\Http\ResponseTrait');
-        $loader->alias('Royalcms\Component\Http\Testing\File', 'Illuminate\Http\Testing\File');
-        $loader->alias('Royalcms\Component\Http\Testing\FileFactory', 'Illuminate\Http\Testing\FileFactory');
-        $loader->alias('Royalcms\Component\Http\Testing\MimeType', 'Illuminate\Http\Testing\MimeType');
-        $loader->alias('Royalcms\Component\Http\UploadedFile', 'Illuminate\Http\UploadedFile');
+
+        foreach (self::aliases() as $class => $alias) {
+            $loader->alias($class, $alias);
+        }
     }
 
+    /**
+     * Load the alias = One less install step for the user
+     */
+    public static function aliases()
+    {
+
+        return [
+            'Royalcms\Component\Http\File'                                     => 'Illuminate\Http\File',
+            'Royalcms\Component\Http\FileHelpers'                              => 'Illuminate\Http\FileHelpers',
+            'Royalcms\Component\Http\JsonResponse'                             => 'Illuminate\Http\JsonResponse',
+            'Royalcms\Component\Http\Middleware\CheckResponseForModifications' => 'Illuminate\Http\Middleware\CheckResponseForModifications',
+            'Royalcms\Component\Http\Middleware\FrameGuard'                    => 'Illuminate\Http\Middleware\FrameGuard',
+            'Royalcms\Component\Http\RedirectResponse'                         => 'Illuminate\Http\RedirectResponse',
+            'Royalcms\Component\Http\Request'                                  => 'Illuminate\Http\Request',
+            'Royalcms\Component\Http\Response'                                 => 'Illuminate\Http\Response',
+            'Royalcms\Component\Http\ResponseTrait'                            => 'Illuminate\Http\ResponseTrait',
+            'Royalcms\Component\Http\Testing\File'                             => 'Illuminate\Http\Testing\File',
+            'Royalcms\Component\Http\Testing\FileFactory'                      => 'Illuminate\Http\Testing\FileFactory',
+            'Royalcms\Component\Http\Testing\MimeType'                         => 'Illuminate\Http\Testing\MimeType',
+            'Royalcms\Component\Http\UploadedFile'                             => 'Illuminate\Http\UploadedFile'
+        ];
+    }
 
 }
