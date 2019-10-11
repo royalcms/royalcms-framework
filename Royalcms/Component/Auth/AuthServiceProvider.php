269ddfc560cec8cbfb1ce56b380584b9684dea9f
王正东 <wangzhengdong@ecmoban.com>
2019-10-11 11:50:11 +0800

    优化Auth组件的别名加载

--- Royalcms/Component/Auth/AuthServiceProvider.php
+++ Royalcms/Component/Auth/AuthServiceProvider.php
@@ -7,15 +7,13 @@ class AuthServiceProvider extends \Illuminate\Auth\AuthServiceProvider
 {
     /**
      * The application instance.
-     *
      * @var \Royalcms\Component\Contracts\Foundation\Royalcms
      */
     protected $royalcms;
 
     /**
      * Create a new service provider instance.
-     *
-     * @param  \Royalcms\Component\Contracts\Foundation\Royalcms  $royalcms
+     * @param \Royalcms\Component\Contracts\Foundation\Royalcms $royalcms
      * @return void
      */
     public function __construct($royalcms)
@@ -27,7 +25,6 @@ class AuthServiceProvider extends \Illuminate\Auth\AuthServiceProvider
 
     /**
      * Register the service provider.
-     *
      * @return void
      */
     public function register()
@@ -42,24 +39,35 @@ class AuthServiceProvider extends \Illuminate\Auth\AuthServiceProvider
      */
     protected function loadAlias()
     {
-        $this->royalcms->booting(function() {
-            $loader = \Royalcms\Component\Foundation\AliasLoader::getInstance();
-            $loader->alias('Royalcms\Component\Auth\GenericUser', 'Illuminate\Auth\GenericUser');
-            $loader->alias('Royalcms\Component\Auth\DatabaseUserProvider', 'Illuminate\Auth\DatabaseUserProvider');
-            $loader->alias('Royalcms\Component\Auth\EloquentUserProvider', 'Illuminate\Auth\EloquentUserProvider');
-            $loader->alias('Royalcms\Component\Auth\GeneratorServiceProvider', 'Illuminate\Auth\GeneratorServiceProvider');
-            $loader->alias('Royalcms\Component\Auth\AuthManager', 'Illuminate\Auth\AuthManager');
-            $loader->alias('Royalcms\Component\Auth\Authenticatable', 'Illuminate\Auth\Authenticatable');
-            $loader->alias('Royalcms\Component\Auth\Passwords\CanResetPassword', 'Illuminate\Auth\Passwords\CanResetPassword');
-            $loader->alias('Royalcms\Component\Auth\Passwords\DatabaseTokenRepository', 'Illuminate\Auth\Passwords\DatabaseTokenRepository');
-            $loader->alias('Royalcms\Component\Auth\Passwords\PasswordBroker', 'Illuminate\Auth\Passwords\PasswordBroker');
-            $loader->alias('Royalcms\Component\Auth\Passwords\PasswordResetServiceProvider', 'Illuminate\Auth\Passwords\PasswordResetServiceProvider');
-            $loader->alias('Royalcms\Component\Auth\Passwords\TokenRepositoryInterface', 'Illuminate\Auth\Passwords\TokenRepositoryInterface');
-            $loader->alias('Royalcms\Component\Auth\Middleware\AuthenticateWithBasicAuth', 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth');
-            $loader->alias('Royalcms\Component\Auth\Access\Gate', 'Illuminate\Auth\Access\Gate');
-            $loader->alias('Royalcms\Component\Auth\Access\HandlesAuthorization', 'Illuminate\Auth\Access\HandlesAuthorization');
-            $loader->alias('Royalcms\Component\Auth\Access\Response', 'Illuminate\Auth\Access\Response');
-        });
+        $loader = \Royalcms\Component\Foundation\AliasLoader::getInstance();
+
+        foreach (self::aliases() as $class => $alias) {
+            $loader->alias($class, $alias);
+        }
+    }
+
+    /**
+     * Load the alias = One less install step for the user
+     */
+    public static function aliases()
+    {
+        return [
+            'Royalcms\Component\Auth\GenericUser'                            => 'Illuminate\Auth\GenericUser',
+            'Royalcms\Component\Auth\DatabaseUserProvider'                   => 'Illuminate\Auth\DatabaseUserProvider',
+            'Royalcms\Component\Auth\EloquentUserProvider'                   => 'Illuminate\Auth\EloquentUserProvider',
+            'Royalcms\Component\Auth\GeneratorServiceProvider'               => 'Illuminate\Auth\GeneratorServiceProvider',
+            'Royalcms\Component\Auth\AuthManager'                            => 'Illuminate\Auth\AuthManager',
+            'Royalcms\Component\Auth\Authenticatable'                        => 'Illuminate\Auth\Authenticatable',
+            'Royalcms\Component\Auth\Passwords\CanResetPassword'             => 'Illuminate\Auth\Passwords\CanResetPassword',
+            'Royalcms\Component\Auth\Passwords\DatabaseTokenRepository'      => 'Illuminate\Auth\Passwords\DatabaseTokenRepository',
+            'Royalcms\Component\Auth\Passwords\PasswordBroker'               => 'Illuminate\Auth\Passwords\PasswordBroker',
+            'Royalcms\Component\Auth\Passwords\PasswordResetServiceProvider' => 'Illuminate\Auth\Passwords\PasswordResetServiceProvider',
+            'Royalcms\Component\Auth\Passwords\TokenRepositoryInterface'     => 'Illuminate\Auth\Passwords\TokenRepositoryInterface',
+            'Royalcms\Component\Auth\Middleware\AuthenticateWithBasicAuth'   => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
+            'Royalcms\Component\Auth\Access\Gate'                            => 'Illuminate\Auth\Access\Gate',
+            'Royalcms\Component\Auth\Access\HandlesAuthorization'            => 'Illuminate\Auth\Access\HandlesAuthorization',
+            'Royalcms\Component\Auth\Access\Response'                        => 'Illuminate\Auth\Access\Response',
+        ];
     }
 
 }
