d8fb0568eec6855a5a48dfe7bccf9e860ecbbdd1
王正东 <wangzhengdong@ecmoban.com>
2019-10-11 11:51:47 +0800

    优化Notifications组件的别名加载

--- Royalcms/Component/Notifications/NotificationServiceProvider.php
+++ Royalcms/Component/Notifications/NotificationServiceProvider.php
@@ -8,15 +8,13 @@ class NotificationServiceProvider extends \Illuminate\Notifications\Notification
 
     /**
      * The application instance.
-     *
      * @var \Royalcms\Component\Contracts\Foundation\Royalcms
      */
     protected $royalcms;
 
     /**
      * Create a new service provider instance.
-     *
-     * @param  \Royalcms\Component\Contracts\Foundation\Royalcms  $royalcms
+     * @param \Royalcms\Component\Contracts\Foundation\Royalcms $royalcms
      * @return void
      */
     public function __construct($royalcms)
@@ -28,7 +26,6 @@ class NotificationServiceProvider extends \Illuminate\Notifications\Notification
 
     /**
      * Register the service provider.
-     *
      * @return void
      */
     public function register()
@@ -47,34 +44,45 @@ class NotificationServiceProvider extends \Illuminate\Notifications\Notification
      */
     protected function loadAlias()
     {
-        $this->royalcms->booting(function() {
-            $loader = \Royalcms\Component\Foundation\AliasLoader::getInstance();
-            $loader->alias('Royalcms\Component\Notifications\SendQueuedNotifications', 'Illuminate\Notifications\SendQueuedNotifications');
-            $loader->alias('Royalcms\Component\Notifications\RoutesNotifications', 'Illuminate\Notifications\RoutesNotifications');
-            $loader->alias('Royalcms\Component\Notifications\Notification', 'Illuminate\Notifications\Notification');
-            $loader->alias('Royalcms\Component\Notifications\Notifiable', 'Illuminate\Notifications\Notifiable');
-            $loader->alias('Royalcms\Component\Notifications\HasDatabaseNotifications', 'Illuminate\Notifications\HasDatabaseNotifications');
-            $loader->alias('Royalcms\Component\Notifications\DatabaseNotificationCollection', 'Illuminate\Notifications\DatabaseNotificationCollection');
-            $loader->alias('Royalcms\Component\Notifications\DatabaseNotification', 'Illuminate\Notifications\DatabaseNotification');
-            $loader->alias('Royalcms\Component\Notifications\ChannelManager', 'Illuminate\Notifications\ChannelManager');
-            $loader->alias('Royalcms\Component\Notifications\Action', 'Illuminate\Notifications\Action');
-            $loader->alias('Royalcms\Component\Notifications\Channels\BroadcastChannel', 'Illuminate\Notifications\Channels\BroadcastChannel');
-            $loader->alias('Royalcms\Component\Notifications\Channels\DatabaseChannel', 'Illuminate\Notifications\Channels\DatabaseChannel');
-            $loader->alias('Royalcms\Component\Notifications\Channels\MailChannel', 'Illuminate\Notifications\Channels\MailChannel');
-            $loader->alias('Royalcms\Component\Notifications\Events\BroadcastNotificationCreated', 'Illuminate\Notifications\Events\BroadcastNotificationCreated');
-            $loader->alias('Royalcms\Component\Notifications\Events\NotificationFailed', 'Illuminate\Notifications\Events\NotificationFailed');
-            $loader->alias('Royalcms\Component\Notifications\Events\NotificationSending', 'Illuminate\Notifications\Events\NotificationSending');
-            $loader->alias('Royalcms\Component\Notifications\Events\NotificationSent', 'Illuminate\Notifications\Events\NotificationSent');
-            $loader->alias('Royalcms\Component\Notifications\Messages\BroadcastMessage', 'Illuminate\Notifications\Messages\BroadcastMessage');
-            $loader->alias('Royalcms\Component\Notifications\Messages\DatabaseMessage', 'Illuminate\Notifications\Messages\DatabaseMessage');
-            $loader->alias('Royalcms\Component\Notifications\Messages\MailMessage', 'Illuminate\Notifications\Messages\MailMessage');
-            $loader->alias('Royalcms\Component\Notifications\Messages\SimpleMessage', 'Illuminate\Notifications\Messages\SimpleMessage');
-        });
+        $loader = \Royalcms\Component\Foundation\AliasLoader::getInstance();
+
+        foreach (self::aliases() as $class => $alias) {
+            $loader->alias($class, $alias);
+        }
     }
-    
+
+    /**
+     * Load the alias = One less install step for the user
+     */
+    public static function aliases()
+    {
+
+        return [
+            'Royalcms\Component\Notifications\SendQueuedNotifications'             => 'Illuminate\Notifications\SendQueuedNotifications',
+            'Royalcms\Component\Notifications\RoutesNotifications'                 => 'Illuminate\Notifications\RoutesNotifications',
+            'Royalcms\Component\Notifications\Notification'                        => 'Illuminate\Notifications\Notification',
+            'Royalcms\Component\Notifications\Notifiable'                          => 'Illuminate\Notifications\Notifiable',
+            'Royalcms\Component\Notifications\HasDatabaseNotifications'            => 'Illuminate\Notifications\HasDatabaseNotifications',
+            'Royalcms\Component\Notifications\DatabaseNotificationCollection'      => 'Illuminate\Notifications\DatabaseNotificationCollection',
+            'Royalcms\Component\Notifications\DatabaseNotification'                => 'Illuminate\Notifications\DatabaseNotification',
+            'Royalcms\Component\Notifications\ChannelManager'                      => 'Illuminate\Notifications\ChannelManager',
+            'Royalcms\Component\Notifications\Action'                              => 'Illuminate\Notifications\Action',
+            'Royalcms\Component\Notifications\Channels\BroadcastChannel'           => 'Illuminate\Notifications\Channels\BroadcastChannel',
+            'Royalcms\Component\Notifications\Channels\DatabaseChannel'            => 'Illuminate\Notifications\Channels\DatabaseChannel',
+            'Royalcms\Component\Notifications\Channels\MailChannel'                => 'Illuminate\Notifications\Channels\MailChannel',
+            'Royalcms\Component\Notifications\Events\BroadcastNotificationCreated' => 'Illuminate\Notifications\Events\BroadcastNotificationCreated',
+            'Royalcms\Component\Notifications\Events\NotificationFailed'           => 'Illuminate\Notifications\Events\NotificationFailed',
+            'Royalcms\Component\Notifications\Events\NotificationSending'          => 'Illuminate\Notifications\Events\NotificationSending',
+            'Royalcms\Component\Notifications\Events\NotificationSent'             => 'Illuminate\Notifications\Events\NotificationSent',
+            'Royalcms\Component\Notifications\Messages\BroadcastMessage'           => 'Illuminate\Notifications\Messages\BroadcastMessage',
+            'Royalcms\Component\Notifications\Messages\DatabaseMessage'            => 'Illuminate\Notifications\Messages\DatabaseMessage',
+            'Royalcms\Component\Notifications\Messages\MailMessage'                => 'Illuminate\Notifications\Messages\MailMessage',
+            'Royalcms\Component\Notifications\Messages\SimpleMessage'              => 'Illuminate\Notifications\Messages\SimpleMessage'
+        ];
+    }
+
     /**
      * Get the services provided by the provider.
-     *
      * @return array
      */
     public function provides()
