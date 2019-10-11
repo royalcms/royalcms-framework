8c7793f38973e55e546887ac55f15353ef013d47
王正东 <wangzhengdong@ecmoban.com>
2019-10-11 11:51:55 +0800

    优化Pagination组件的别名加载

--- Royalcms/Component/Pagination/PaginationServiceProvider.php
+++ Royalcms/Component/Pagination/PaginationServiceProvider.php
@@ -7,15 +7,13 @@ class PaginationServiceProvider extends \Illuminate\Pagination\PaginationService
 
     /**
      * The application instance.
-     *
      * @var \Royalcms\Component\Contracts\Foundation\Royalcms
      */
     protected $royalcms;
 
     /**
      * Create a new service provider instance.
-     *
-     * @param  \Royalcms\Component\Contracts\Foundation\Royalcms  $royalcms
+     * @param \Royalcms\Component\Contracts\Foundation\Royalcms $royalcms
      * @return void
      */
     public function __construct($royalcms)
@@ -27,7 +25,6 @@ class PaginationServiceProvider extends \Illuminate\Pagination\PaginationService
 
     /**
      * Register the service provider.
-     *
      * @return void
      */
     public function register()
@@ -42,13 +39,25 @@ class PaginationServiceProvider extends \Illuminate\Pagination\PaginationService
      */
     protected function loadAlias()
     {
-        $this->royalcms->booting(function() {
-            $loader = \Royalcms\Component\Foundation\AliasLoader::getInstance();
-            $loader->alias('Royalcms\Component\Pagination\AbstractPaginator', 'Illuminate\Pagination\AbstractPaginator');
-            $loader->alias('Royalcms\Component\Pagination\LengthAwarePaginator', 'Illuminate\Pagination\LengthAwarePaginator');
-            $loader->alias('Royalcms\Component\Pagination\Paginator', 'Illuminate\Pagination\Paginator');
-            $loader->alias('Royalcms\Component\Pagination\UrlWindow', 'Illuminate\Pagination\UrlWindow');
-        });
+        $loader = \Royalcms\Component\Foundation\AliasLoader::getInstance();
+
+        foreach (self::aliases() as $class => $alias) {
+            $loader->alias($class, $alias);
+        }
+    }
+
+    /**
+     * Load the alias = One less install step for the user
+     */
+    public static function aliases()
+    {
+
+        return [
+            'Royalcms\Component\Pagination\AbstractPaginator'    => 'Illuminate\Pagination\AbstractPaginator',
+            'Royalcms\Component\Pagination\LengthAwarePaginator' => 'Illuminate\Pagination\LengthAwarePaginator',
+            'Royalcms\Component\Pagination\Paginator'            => 'Illuminate\Pagination\Paginator',
+            'Royalcms\Component\Pagination\UrlWindow'            => 'Illuminate\Pagination\UrlWindow'
+        ];
     }
 
 
