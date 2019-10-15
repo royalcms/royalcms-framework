6fb34d9810426ae6fcec4c6edb535eb231577136
王正东 <wangzhengdong@ecmoban.com>
2019-10-15 09:06:57 +0800

    优化Facade继承父类

--- Royalcms/Component/Support/Facades/Facade.php
+++ Royalcms/Component/Support/Facades/Facade.php
@@ -6,7 +6,7 @@ use Mockery;
 use RuntimeException;
 use Mockery\MockInterface;
 
-abstract class Facade
+abstract class Facade extends \Illuminate\Support\Facades\Facade
 {
     /**
      * The royalcms instance being facaded.
@@ -14,13 +14,6 @@ abstract class Facade
      * @var \Royalcms\Component\Contracts\Foundation\Royalcms
      */
     protected static $royalcms;
-
-    /**
-     * The resolved object instances.
-     *
-     * @var array
-     */
-    protected static $resolvedInstance;
 
     /**
      * Hotswap the underlying instance behind the facade.
@@ -32,104 +25,9 @@ abstract class Facade
     {
         static::$resolvedInstance[static::getFacadeAccessor()] = $instance;
 
-        static::$royalcms->instance(static::getFacadeAccessor(), $instance);
-    }
-
-    /**
-     * Initiate a mock expectation on the facade.
-     *
-     * @param  mixed
-     * @return \Mockery\Expectation
-     */
-    public static function shouldReceive()
-    {
-        $name = static::getFacadeAccessor();
-
-        if (static::isMock()) {
-            $mock = static::$resolvedInstance[$name];
-        } else {
-            $mock = static::createFreshMockInstance($name);
-        }
-
-        return call_user_func_array([$mock, 'shouldReceive'], func_get_args());
-    }
-
-    /**
-     * Create a fresh mock instance for the given class.
-     *
-     * @param  string  $name
-     * @return \Mockery\Expectation
-     */
-    protected static function createFreshMockInstance($name)
-    {
-        static::$resolvedInstance[$name] = $mock = static::createMockByName($name);
-
-        $mock->shouldAllowMockingProtectedMethods();
-
         if (isset(static::$royalcms)) {
-            static::$royalcms->instance($name, $mock);
+            static::$royalcms->instance(static::getFacadeAccessor(), $instance);
         }
-
-        return $mock;
-    }
-
-    /**
-     * Create a fresh mock instance for the given class.
-     *
-     * @param  string  $name
-     * @return \Mockery\Expectation
-     */
-    protected static function createMockByName($name)
-    {
-        $class = static::getMockableClass($name);
-
-        return $class ? Mockery::mock($class) : Mockery::mock();
-    }
-
-    /**
-     * Determines whether a mock is set as the instance of the facade.
-     *
-     * @return bool
-     */
-    protected static function isMock()
-    {
-        $name = static::getFacadeAccessor();
-
-        return isset(static::$resolvedInstance[$name]) && static::$resolvedInstance[$name] instanceof MockInterface;
-    }
-
-    /**
-     * Get the mockable class for the bound instance.
-     *
-     * @return string|null
-     */
-    protected static function getMockableClass()
-    {
-        if ($root = static::getFacadeRoot()) {
-            return get_class($root);
-        }
-    }
-
-    /**
-     * Get the root object behind the facade.
-     *
-     * @return mixed
-     */
-    public static function getFacadeRoot()
-    {
-        return static::resolveFacadeInstance(static::getFacadeAccessor());
-    }
-
-    /**
-     * Get the registered name of the component.
-     *
-     * @return string
-     *
-     * @throws \RuntimeException
-     */
-    protected static function getFacadeAccessor()
-    {
-        throw new RuntimeException('Facade does not implement getFacadeAccessor method.');
     }
 
     /**
@@ -152,27 +50,6 @@ abstract class Facade
     }
 
     /**
-     * Clear a resolved facade instance.
-     *
-     * @param  string  $name
-     * @return void
-     */
-    public static function clearResolvedInstance($name)
-    {
-        unset(static::$resolvedInstance[$name]);
-    }
-
-    /**
-     * Clear all of the resolved instances.
-     *
-     * @return void
-     */
-    public static function clearResolvedInstances()
-    {
-        static::$resolvedInstance = [];
-    }
-
-    /**
      * Get the application instance behind the facade.
      *
      * @return \Royalcms\Component\Contracts\Foundation\Royalcms
@@ -191,6 +68,7 @@ abstract class Facade
     public static function setFacadeRoyalcms($royalcms)
     {
         static::$royalcms = $royalcms;
+        static::$app = $royalcms;
     }
 
     /**
