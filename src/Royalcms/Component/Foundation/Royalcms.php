a38a9c7cd864c89b9bd057ed1f5a8170a8ba465f
王正东 <wangzhengdong@ecmoban.com>
2020-04-21 14:57:55 +0800

    update version to v7.1.0

--- src/Royalcms/Component/Foundation/Royalcms.php
+++ src/Royalcms/Component/Foundation/Royalcms.php
@@ -28,14 +28,14 @@ class Royalcms extends Application implements RoyalcmsContract, ContainerContrac
      *
      * @var string
      */
-    const VERSION = '7.0.0';
+    const VERSION = '7.1.0';
 
     /**
      * The Royalcms framework release.
      *
      * @var string
      */
-    const RELEASE = '2020-03-12';
+    const RELEASE = '2020-04-21';
 
     /**
      * A custom callback used to configure Monolog.
