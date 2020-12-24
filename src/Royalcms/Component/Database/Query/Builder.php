862f97cd72653ad08fbad44be4489640a979623e
王正东 <wangzhengdong@ecmoban.com>
2020-12-24 13:45:13 +0800

    调整RC_DB的first()、aggregate()方法返回与laravel一致

--- src/Royalcms/Component/Database/Query/Builder.php
+++ src/Royalcms/Component/Database/Query/Builder.php
@@ -21,37 +21,37 @@ class Builder extends QueryBuilder
 //        return parent::get($columns)->all();
 //    }
 
-    /**
-     * Execute an aggregate function on the database.
-     *
-     * @param  string  $function
-     * @param  array   $columns
-     * @return mixed
-     */
-    public function aggregate($function, $columns = ['*'])
-    {
-        $results = $this->cloneWithout($this->unions ? [] : ['columns'])
-            ->cloneWithoutBindings($this->unions ? [] : ['select'])
-            ->setAggregate($function, $columns)
-            ->get($columns);
+//    /**
+//     * Execute an aggregate function on the database.
+//     *
+//     * @param  string  $function
+//     * @param  array   $columns
+//     * @return mixed
+//     */
+//    public function aggregate($function, $columns = ['*'])
+//    {
+//        $results = $this->cloneWithout($this->unions ? [] : ['columns'])
+//            ->cloneWithoutBindings($this->unions ? [] : ['select'])
+//            ->setAggregate($function, $columns)
+//            ->get($columns);
+//
+//        if (isset($results[0])) {
+//            return array_change_key_case((array) $results[0])['aggregate'];
+//        }
+//    }
 
-        if (isset($results[0])) {
-            return array_change_key_case((array) $results[0])['aggregate'];
-        }
-    }
-
-    /**
-     * Execute the query and get the first result.
-     *
-     * @param  array  $columns
-     * @return \Illuminate\Database\Eloquent\Model|object|static|null
-     */
-    public function first($columns = ['*'])
-    {
-        $results = $this->take(1)->get($columns);
-
-        return count($results) > 0 ? reset($results) : null;
-    }
+//    /**
+//     * Execute the query and get the first result.
+//     *
+//     * @param  array  $columns
+//     * @return \Illuminate\Database\Eloquent\Model|object|static|null
+//     */
+//    public function first($columns = ['*'])
+//    {
+//        $results = $this->take(1)->get($columns)->all();
+//
+//        return count($results) > 0 ? reset($results) : null;
+//    }
 
     /**
      * Get an array with the values of a given column.
